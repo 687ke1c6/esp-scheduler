920e249d35ace0c2873d7ef26d1ca051150a6be7
687ke1c6 <mstdtibe@gmail.com>
2024-02-12 15:24:37 +0000

    Filter out start events before now

--- src/main.rs
+++ src/main.rs
@@ -4,12 +4,17 @@ use chrono::Utc;
 use models::area_information::AreaInformation;
 use tokio::process::Command;
 
-use crate::models::config::Config;
+use crate::models::{area_information::Event, config::Config};
 
 mod models;
 
+fn log(log_line: String) {
+    println!("{} | {}", Utc::now().format("%d-%m-%Y %H:%M:%S"), log_line);
+}
+
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    log(format!("starting esp-scheduler"));
     let mut file = File::open("esp-scheduler.yaml").expect("Could not open config file");
     let mut file_contents = String::new();
 
@@ -19,17 +24,17 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
         Config::from_str(&file_contents).expect("Could not deserialize configuration");
 
     let interval_secs = u64::from(configuration.interval_mins.as_ref().unwrap_or(&1) * 60);
-
     let mut interval = tokio::time::interval(Duration::from_secs(interval_secs));
+
     let http_client = reqwest::Client::new();
-    let mut count = 0;
     let mut cached_body = String::from("{\"events\" : []}");
     let fetch_mins = configuration.fetch_occurrence.unwrap_or_else(|| 60);
 
+    let mut count = 0;
     loop {
         interval.tick().await;
         if count % fetch_mins == 0 {
-            println!("{}, {}", "fetching", &configuration.fetch.url);
+            log(format!("{}, {}", "fetching", &configuration.fetch.url));
             let mut request = http_client.get(&configuration.fetch.url);
 
             // apply headers
@@ -42,12 +47,15 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
             // send reqwest
             let response = request.send().await;
             if let Err(err) = response {
-                println!("{}\n{}", "Unable to reach host", err);
+                log(format!("{}\n{}", "Unable to reach host", err));
                 continue;
             }
             let text = response.unwrap().text().await;
             if let Err(err) = text {
-                println!("{}\n{}", "Could not read body of http response", err);
+                log(format!(
+                    "{}\n{}",
+                    "Could not read body of http response", err
+                ));
                 continue;
             }
             cached_body = text.unwrap();
@@ -55,28 +63,39 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
 
         let area_information = AreaInformation::new(&cached_body)?;
 
-        let mut events = area_information.events;
+        let mut events: Vec<&Event> = area_information
+            .events
+            .iter()
+            .filter(|e| e.start.to_utc() > Utc::now())
+            .collect();
+
         events.sort_by_key(|a| a.start);
 
         if let Some(first) = events.first() {
             let time_diff = first.start.to_utc() - Utc::now();
-            println!("{} in {} mins", first.note, time_diff.num_minutes());
-            if time_diff.num_minutes() < 0 {
-                println!(
-                    "WARNING: {} mins left\n\tnext: {}\n\tnow: {}",
-                    time_diff.num_minutes(),
-                    first.start.to_utc(),
-                    Utc::now()
-                );
-            } else if time_diff.num_minutes() < configuration.threshold.into() {
-                println!("{}: {} mins", "threshold reached", configuration.threshold);
+
+            log(format!(
+                "{} in {} mins. {}",
+                first.note,
+                time_diff.num_minutes(),
+                if time_diff.num_hours() <= 1 {
+                    format!("")
+                } else {
+                    format!("(~{} hrs)", time_diff.num_hours())
+                }
+            ));
+            if time_diff.num_minutes() < configuration.threshold.into() {
+                log(format!(
+                    "{}: {} mins",
+                    "threshold reached", configuration.threshold
+                ));
                 let commands = configuration
                     .commands
                     .as_ref() // convert from &Option<T> to Option<&T>
                     .map(|v| &v[..]) // map to slice instead of a container reference
                     .unwrap_or_else(|| &[]); // otherwise return an empty slice.
                 for command in commands {
-                    println!("executing command: {}", command);
+                    log(format!("executing command: {}", command));
                     let child = Command::new("sh")
                         .args(["-c", command])
                         .current_dir(".")
@@ -84,7 +103,7 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
                         .wait()
                         .await;
                     if let Err(err) = child {
-                        println!("{}\n{}", "could not execute command", err);
+                        log(format!("{}\n{}", "could not execute command", err));
                     }
                 }
             }
